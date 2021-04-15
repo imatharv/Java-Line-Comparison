cf121b5a220f35c296f7636cd03dff09c43f4551
Atharva Joshi <iamatharvaj@google.com>
2021-04-16 00:03:45 +0530

    Added Solution for use-case-4

--- lineComparison.java
+++ lineComparison.java
@@ -1,56 +1,63 @@
 import java.util.Scanner;
+import java.lang.*;
 
-public class lineComparison
+public class lineComparison 
 {
-	public double lineLength()
+	public double lineLength() 
 	{
-   		Scanner scanner = new Scanner(System.in);
-      		double x1;
-      		double y1;
-      		double x2;
-      		double y2;
-      		
-      		System.out.println("Enter value for x1: ");
-      		x1 = scanner.nextDouble();
+		Scanner scanner = new Scanner(System.in);
+		double x1;
+		double y1;
+		double x2;
+		double y2;
+
+		System.out.println("Enter value for x1: ");
+		x1 = scanner.nextDouble();
 
 		System.out.println("Enter value for y1: ");
-      		y1 = scanner.nextDouble();
+		y1 = scanner.nextDouble();
 
 		System.out.println("Enter value for x2: ");
-      		x2 = scanner.nextDouble();
+		x2 = scanner.nextDouble();
 
 		System.out.println("Enter value for y2: ");
-      		y2 = scanner.nextDouble();
+		y2 = scanner.nextDouble();
 
-      		return Math.sqrt((Math.pow(x2 - x1, 2)) + (Math.pow(y2 - y1, 2)));
-   	}
-	public static void main(String[] args)
+		return Math.sqrt((Math.pow(x2 - x1, 2)) + (Math.pow(y2 - y1, 2)));
+	}
+
+	public static void main(String[] args) 
 	{
-  		System.out.println("Welcome to the line comparison problem");
+		System.out.println("Welcome to the line comparison problem");
 
 		lineComparison object = new lineComparison();
 
 		System.out.println("Enter co-ordinates for first line: ");
-      		double firstLineLength = object.lineLength();
-      		System.out.println("Length of first line is: " +firstLineLength);
+		double firstLineLength = object.lineLength();
+		System.out.println("Length of first line is: " + firstLineLength);
 
-      		System.out.println("Enter co-ordinates for second line: ");
-      		double secondLineLength = object.lineLength();
-      		System.out.println("Length of second line is: " +secondLineLength);
+		System.out.println("Enter co-ordinates for second line: ");
+		double secondLineLength = object.lineLength();
+		System.out.println("Length of second line is: " + secondLineLength);
 
-		if (firstLineLength==secondLineLength)
+		Double obj1 = new Double(firstLineLength);
+      		Double obj2 = new Double(secondLineLength);
+
+		if (obj1.equals(obj2)) 
 		{
-      			System.out.println("Both the lines are equal.");
-      		}
-	   	else
-	   	{
-      			System.out.println("Both the lines are not equal.");
+			System.out.println("Both the lines are equal.");
+		} 
+		else 
+		{
+			System.out.println("Both the lines are not equal.");
 
-			if (firstLineLength > secondLineLength) 
+      			int outcome =  obj1.compareTo(obj2);
+			
+			if (outcome > 0) 
 			{
 				System.out.println("First line is longer than second one.");
-		        }
-			else
+			} 
+			else if(outcome < 0) 
 			{
 				System.out.println("Second line is longer than first one.");
 			}
